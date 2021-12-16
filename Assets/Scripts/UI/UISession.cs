3665d84c15745ca5a9a1a9759d91c41b7caa1f5a
devnem0y <devnem0y@gmail.com>
2021-12-16 17:30:56 +0500

    Анимация Fade

--- Assets/Scripts/UI/UISession.cs
+++ Assets/Scripts/UI/UISession.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,6 +7,8 @@ public class UISession : MonoBehaviour
     [SerializeField] private Text _labelTask;
     [SerializeField] private Image _imageFade;
     [SerializeField] private Button _btnRestart;
+    
+    private Tween _fadeTween;
 
     private void Awake()
     {
@@ -24,19 +27,34 @@ public class UISession : MonoBehaviour
     private void Win()
     {
         _imageFade.gameObject.SetActive(true);
-        _btnRestart.gameObject.SetActive(true);
+        Fade(_imageFade, 0.75f, 0.6f, () =>
+        {
+            _btnRestart.gameObject.SetActive(true); 
+        });
     }
     
     private void Launch()
     {
         _imageFade.gameObject.SetActive(false);
+        Fade(_imageFade, 0f, 1.5f, () => {});
     }
     
     private void Restart()
     {
-        _imageFade.gameObject.SetActive(false);
         _btnRestart.gameObject.SetActive(false);
-        Dispatcher.Send(Event.ON_RESTART);
+        Fade(_imageFade, 1f, 0.3f, () =>
+        {
+            _imageFade.gameObject.SetActive(false);
+            Dispatcher.Send(Event.ON_RESTART);
+        });
+    }
+
+    private void Fade(Image image, float endValue, float duration, TweenCallback onEnd)
+    {
+        _fadeTween?.Kill();
+
+        _fadeTween = image.DOFade(endValue, duration);
+        _fadeTween.onComplete += onEnd;
     }
 
     private void OnDestroy()
