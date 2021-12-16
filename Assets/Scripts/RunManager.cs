b9500048a65197bb4f9dc2f7f2469103d8f5130b
devnem0y <devnem0y@gmail.com>
2021-12-16 17:30:36 +0500

    Рефакторинг

--- Assets/Scripts/RunManager.cs
+++ Assets/Scripts/RunManager.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 
 public class RunManager : MonoBehaviour
 {
+    //TODO: Возможно не лучшее решение, но лучше не придумал, чтобы запустить карутину из ScriptableObject
     public static RunManager instance;
     
     [SerializeField] private Session _session;
 
     private void Start()
     {
-        RunManager.instance = this;
+        instance = this;
 
         DOTween.Init();
         _session.Subscribe();
